735f5ff50cb599a35f1766bd3109babc04273ba4
jose muthuan <josemuthuan@gmail.com>
2023-11-08 16:18:48 -0300

    change fetching data from local to  server

--- utils/getData.js
+++ utils/getData.js
@@ -4,7 +4,7 @@ const getData = async (query) => {
     const API_URL = 'https://midu-store-api.onrender.com/api/items'
 
     try {
-        const data = await fetch(`${API_URL_LOCAL}?q=${query}`)
+        const data = await fetch(`${API_URL}?q=${query}`)
             .then(response => response.json())
             //.then(data => )
         return data;    
--- utils/getDataDetails.js
+++ utils/getDataDetails.js
@@ -3,7 +3,7 @@ const getDataDetails = async (id) => {
         const API_URL = 'https://midu-store-api.onrender.com/api/items'
 
         try {
-                const details = fetch(`${API_URL_LOCAL}/${id}`)
+                const details = fetch(`${API_URL}/${id}`)
                         .then(response => response.json())
                         .catch(rej => console.log(rej))
                 return details;
